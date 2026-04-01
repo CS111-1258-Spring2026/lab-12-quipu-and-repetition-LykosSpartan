f2d2422fe9b7d3bf7451442636b93f17dd8f9b17
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-04-01 06:23:22 +0000

    The Code works but the test fail for some reason

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 /********************************************
 *	AUTHOR:	
 * COLLABORATORS: 
@@ -29,11 +28,17 @@ CALC onesPlace = remainder % 10
 *********************************************/
 
 
+/**
+ * Main class for the Quipu program.
+ */
 public class Main
 {
 
   /***** CONSTANT SECTION *****/
    public static final int QUIPU_WIDTH = 30;
+  /**
+   * Main method to run the program.
+   */
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
@@ -46,60 +51,53 @@ public class Main
     
     
     /***** INTRO SECTION *****/
-    System.out.println("Hello! This program turns any whole number between 0 and 999 into a digital Quipu.\n\n");
+    System.out.println("Hello! This program turns any whole number between 0 and 999 into a digital Quipu.\n");
 
     Main.programIntro();
     Main.toContinue();
 
-
-    /***** INPUT SECTION *****/
-   
-
-    
-
-   
-
-    /***** PROCESSING SECTION *****/
-    
-
-    
-    
-    /***** OUTPUT SECTION *****/
-      
      
   }
-  /***** STATIC METHODS *****/
-
+ 
+  /**
+   * Prompts the user for a number and displays the quipu.
+   */
 public static void programIntro(){
   int num;
     
-     num = UtilityBelt.readInt("Please enter a number between 0 and 999: ", 0, 999);
+     num = UtilityBelt.readInt("\nPlease enter a number between 0 and 999: ", 0, 999);
     
-    System.out.println("\nHundreds  = " + num / 100);
-    System.out.println("Tens        = " + (num % 100) / 10);
-    System.out.println("Ones        = " + num % 10);
+    System.out.println("Hundreds   = " + num / 100);
+    System.out.println("Tens       = " + (num % 100) / 10);
+    System.out.println("Ones       = " + num % 10);
 
 
-    System.out.println("\nYour quipu is:");
+    System.out.println("\nYour quipu:");
+    
     printQuipu(num / 100, (num % 100) / 10, num % 10);
 
 }
 
 
 
-
+  /**
+   * Prints the specified number of knots.
+   */
 public static void printKnots(int numKnots){
     
   
   for (int i = 0; i < numKnots; i++){
-      UtilityBelt.printCentered((QUIPU_WIDTH), "*");
+      UtilityBelt.printCentered(QUIPU_WIDTH, "*");
 
     }
 }
 
 
+  /**
+   * Prints the quipu for the given hundreds, tens, and ones.
+   */
 public static void printQuipu(int hundreds, int tens, int ones){
-  UtilityBelt.printCentered(QUIPU_WIDTH,  "---");
+  UtilityBelt.printCentered(QUIPU_WIDTH,  "___");
   UtilityBelt.printCentered(QUIPU_WIDTH,  "|");
   Main.printKnots(hundreds);
   UtilityBelt.printCentered(QUIPU_WIDTH,  "|");
@@ -108,19 +106,20 @@ public static void printQuipu(int hundreds, int tens, int ones){
   Main.printKnots(ones);
   UtilityBelt.printCentered(QUIPU_WIDTH,  "|");
   UtilityBelt.printCentered(QUIPU_WIDTH,  "‾");
-
-
 }
 
+  /**
+   * Handles the continuation loop for multiple quipus.
+   */
 public static void toContinue(){
   char response = UtilityBelt.readChar("Would you like to make another quipu? [Y/N]: ", "YyNn");
-  while (response == 'Y') {
+  while (response == 'Y' || response == 'y') {
     Main.programIntro();
     response = UtilityBelt.readChar("Would you like to make another quipu? [Y/N]: ", "YyNn");
   }
-  if (response == 'N') {
-    System.out.println("\nGoodbye!");
-   
+  if (response == 'N' || response == 'n') {
+    
+  
   }
 }
 }
